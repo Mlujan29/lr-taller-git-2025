0afaa9fa4193c3694f2acdcfead494b54ba01168
Lujan Ramirez <rmlujan29@gmail.com>
2025-11-19 09:20:08 -0300

    Correcciones añadidas al archivo promos.py

--- desafio-02/promos.py
+++ desafio-02/promos.py
@@ -3,27 +3,37 @@
 def calcular_descuento(items_de_venta,productos,promociones):
     descuento_total =  0                #acuulador para calcular el descuento total
 
+    items_dict =  {}        #modificacion: para un recorrido mas sencillo hacemos que la lista que viene se convierta en diccionario
+    for (codigo, cantidad) in items_de_venta:
+        items_dict[codigo] = cantidad
+
     for promo in promociones:       #analizamos las promociones
-
-        if promo['tipo'] == "3x2" and promo['producto'] ==  "chicle":
-            for (codigo, cantidad) in items_de_venta:                           #analizamos lo que se vendió
-                if codigo == "chicle":
-                    cantidad_gratis = cantidad // 3         #para que el resultado sea entero
-                    descuento_total +=  cantidad_gratis * productos['chicle']['precio']
         
-        elif promo['tipo'] == "10%" and promo['producto'] ==  "agua500":
-            for (codigo, cantidad) in items_de_venta:       #recorre los productos
-                if codigo == "agua500":
-                    if cantidad >= promo['min_cantidad']:   #solo aplica si es mayor o igual a 5 la compra
-                        sin_descuento = cantidad * productos['agua500']['precio']       #primero calculamos el precio original
-                        descuento_total += sin_descuento * 0.10         #luego aplicamos el descuento
+        tipo_promo = promo['tipo']      # estandarizamos
+        prod_promo = promo.get('producto')  #para cuando es un solo producto  (si hay más se guarda como NONE)
+        
+        if tipo_promo == "3x2":    # modificación: se adapto para el uso del diccionario y la estandarización
+            if prod_promo in items_dict:    #si esta en el diccionario
+                
+                cantidad_vendida = items_dict[prod_promo]       #el valor de la clave en este caso es la cantidad
+                cantidad_gratis = cantidad_vendida // 3     # usamos // para que sea entero el resultado
+                descuento_total += cantidad_gratis * productos[prod_promo]['precio']
+        
+        elif tipo_promo == "10%":       # modificación: se adapto para el uso del diccionario y la estandarización
+            if prod_promo in items_dict:
+                
+                cantidad_vendida = items_dict[prod_promo]
+                if cantidad_vendida >= promo['min_cantidad']:               # Aca usamos 'min_cantidad' directo del diccionario de promociones
+                    monto_producto = cantidad_vendida * productos[prod_promo]['precio']
+                    descuento_total += monto_producto * 0.10   #se aplica el 10%
 
-        elif promo['tipo'] == "combo_fijo":
-            codigos = []        #para apartar los codigos de cada producto vendido
-            for (codigo,cantidad) in items_de_venta:
-                codigos.append(codigo)      #añadimos los codigos
-            items_promocion = promo['productos']    #traemos los productos del combo para comparar con los que estan en la lista
-            if items_promocion[0] in codigos and items_promocion[1] in codigos:
-                descuento_total += promo['descuento']
+        elif tipo_promo == "combo_fijo":
+            lista_combo = promo['productos'] # Lee ['galleta', 'cafe'] del diccionario de promocion
+            
+            p1 = lista_combo[0]
+            p2 = lista_combo[1]
+            
+            if p1 in items_dict and p2 in items_dict:
+                descuento_total += promo['descuento']   #ve cuanto se tiene que descontar
     
     return int(descuento_total)     #devolvemos el resultado final de descuento
